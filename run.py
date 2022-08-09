2637e3a60d241342db19ead4a5fd23ad12a0b136
Ed Whyatt <lizardxyz@googlemail.com>
2022-08-09 12:38:37 +0000

    Add print stock next market values to terminal for user

--- run.py
+++ run.py
@@ -130,7 +130,50 @@ def main():
     sales_columns = get_last_5_entries_sales()
     stock_data = calculate_stock_data(sales_columns)
     update_worksheet(stock_data, "stock")
+    return stock_data
 
 
 print("Welcome to Love Sandwiches Data Automation")
-main()
+stock_data = main()
+
+
+def get_stock_values(data):
+    """
+    Collects collums of data from sales work sheet, collecting the new
+    snadwiches results
+    """
+    print("Make the following numbers of sandwiches for next market:\n")
+    stock = SHEET.worksheet("stock")
+    
+    headings = []
+    for head in range(1, 7):
+        column = stock.col_values(head)
+        headings.append(column[0])
+
+    return ({headings[i]: data[i] for i in range(len(headings))})
+
+
+stock_values = get_stock_values(stock_data)
+print(stock_values)
+
+
+# student writes function
+# def get_stock_values(data):
+#     """
+#     Print out the calculated stock numbers for each sandwich type.
+#     """
+#     headings = SHEET.worksheet("stock").get_all_values()[0]
+
+#     # headings = SHEET.worksheet('stock').row_values(1)
+
+#     print("Make the following numbers of sandwiches for next market:\n")
+
+#     # new_data = {}
+#     # for heading, stock_num in zip(headings, data):
+#     #     new_data[heading] = stock_num
+#     # return new_data
+    
+#     return {heading: data for heading, data in zip(headings, data)}
+    
+# stock_values = get_stock_values(stock_data)
+# print(stock_values)
